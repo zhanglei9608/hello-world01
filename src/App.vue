c5009b75e320f3ab715d6457e1ecdcb4964f03ca
zhanglei9608 <zl1774998395@163.com>
2021-01-13 00:46:16 +0800

    测试jenkins

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+      <h2>预祝0713java全体学生前程似锦</h2>
+      <h2>预祝0713java全体学生统统月薪过万</h2>
+      <h2>预祝0713java全体学生统统月薪过两万</h2>
+      <h2>预祝0713java全体学生统统月薪过三万</h2>
+      <h2>预祝0713java全体学生统统年薪过三十万</h2>
+   <img src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2941052538,530581840&fm=26&gp=0.jpg"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
