0675d5ed6f72f13bfab6e6db824d1b3514aec1af
zhanglei9608 <zl1774998395@163.com>
2021-01-14 22:48:58 +0800

    测试jenkins

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@
       <h2>预祝0713java全体学生前程似锦</h2>
       <h2>预祝0713java全体学生统统月薪过万</h2>
       <h2>预祝0713java全体学生统统月薪过两万</h2>
-      <h2>预祝0713java全体学生统统月薪过三万</h2>
-      <h2>预祝0713java全体学生统统年薪过三十万</h2>
    <img src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2941052538,530581840&fm=26&gp=0.jpg"/>
   </div>
 </template>
